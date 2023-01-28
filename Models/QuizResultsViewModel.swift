998a016c7026fb673abfecfb0e9bbd2a25e5dc01
Heamex <1909.belov@gmail.com>
2023-01-28 22:08:44 +0300

    Приступил к разделению ответственности
    - Вынес моки в отдельный файл(класс) QuestionFactory.swift
    - Создал этому файлу интерфейс взаимодействия в виде функции requestNextQuestion ()
    - Создал раздел проекта Models и вынес в него:
    - QuizStepViewModel
    - QuizResultsViewModel
    - QuizQuestion
    в виде отдельных файлов.

---
+++ Models/QuizResultsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Nikita Belov on 28.01.23.
+//
+
+import Foundation
+
+/// Вью-модель для состояния "Результат квиза"
+private struct QuizResultsViewModel {
+	let title: String
+	let text: String
+	let buttonText: String
+}
